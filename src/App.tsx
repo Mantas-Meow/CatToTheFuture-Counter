259de22252a3c86698c9f6797d62ca8a4e23687b
Ovidijus Stankaitis <ovidijus.stankaitis@gmail.com>
2022-10-01 05:08:43 +0300

    Added background color, heading and timer to the event

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+
+  const [days, setDays] = useState(0)
+  const [hours, setHours] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+
+useEffect(() =>{
+  const target = new Date("10/13/2022 19:59:59")
+
+  const interval = setInterval(() => {
+    const now = new Date()
+    const difference = target.getTime() - now.getTime()
+
+    const d = Math.floor(difference / (1000 * 60 * 60 * 24))
+    setDays(d)
+
+    const h = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    setHours(h)
+
+    const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+    setMinutes(m)
+
+    const s = Math.floor((difference % (1000 * 60)) / 1000)
+    setSeconds(s)
+
+  }, 1000)
+
+  return () => clearInterval(interval)
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="Heading">ATGAL Į KATEITĮ</span>
+      <span className="Clock">Days: {days}</span>
+      <span className="Clock">Hours: {hours}</span>
+      <span className="Clock">Minutes: {minutes}</span>
+      <span className="Clock">Seconds: {seconds}</span>
     </div>
   );
 }
