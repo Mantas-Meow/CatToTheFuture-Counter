9c6349e339dfbacf13784eea102c37053b49d05d
Ovidijus Stankaitis <ovidijus.stankaitis@gmail.com>
2022-10-01 23:17:17 +0300

    Fixed counter

--- src/App.tsx
+++ src/App.tsx
@@ -8,37 +8,34 @@ const App: React.FC = () => {
   const [minutes, setMinutes] = useState(0)
   const [seconds, setSeconds] = useState(0)
 
-useEffect(() =>{
-  const target = new Date("10/13/2022 19:59:59")
+  useEffect(() =>{
+    const target = new Date("10/13/2022 19:59:59")
 
-  const interval = setInterval(() => {
-    const now = new Date()
-    const difference = target.getTime() - now.getTime()
+    const interval = setInterval(() => {
+      const now = new Date()
+      const difference = target.getTime() - now.getTime()
 
-    const d = Math.floor(difference / (1000 * 60 * 60 * 24))
-    setDays(d)
+      const d = Math.floor(difference / (1000 * 60 * 60 * 24))
+      setDays(d)
 
-    const h = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    setHours(h)
+      const h = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      setHours(h)
 
-    const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
-    setMinutes(m)
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+      setMinutes(m)
 
-    const s = Math.floor((difference % (1000 * 60)) / 1000)
-    setSeconds(s)
+      const s = Math.floor((difference % (1000 * 60)) / 1000)
+      setSeconds(s)
 
-  }, 1000)
+    }, 1000)
 
-  return () => clearInterval(interval)
-}, [])
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <div className="App">
       <span className="Heading">ATGAL Į KATEITĮ</span>
-      <span className="Clock">Days: {days}</span>
-      <span className="Clock">Hours: {hours}</span>
-      <span className="Clock">Minutes: {minutes}</span>
-      <span className="Clock">Seconds: {seconds}</span>
+      <span className="Clock">{days} : {hours} : {minutes} : {seconds}</span>
     </div>
   );
 }
